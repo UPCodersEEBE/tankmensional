510c5c6dbd1455f6b6fc42595f2dcad2a915c68e
clara-9 <38263660+clara-9@users.noreply.github.com>
2020-03-28 18:11:52 +0100

    Uploading improved csv and small data manipulation

--- tankmensional_data.py
+++ tankmensional_data.py
@@ -7,5 +7,25 @@ Created on Sat Mar 28 16:46:09 2020
 
 
 import pandas as pd
+import seaborn as sns
+
+#Constants
+
+ro=1000
+densitat=ro
+viscositat=0.00102
 
 df=pd.read_csv("rowdy.csv", sep=";")
+
+df.rename(columns={'V (rpm)': 'velocitat',"Power (W)":"power"},inplace=True)
+
+df["velocitat"]=df["velocitat"]/60
+
+sns.scatterplot(x=df["velocitat"], y=df["power"])
+
+df["Re"]=ro*df["velocitat"]*df["Rodet Diameter"]/viscositat
+
+df["Np"]=(df["power"]/(df["Rodet Diameter"]**5*df["velocitat"]**3*ro))
+
+
+
