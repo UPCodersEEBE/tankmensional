05733cbf0a2f989596471ec59006820ed32df73b
clara-9 <38263660+clara-9@users.noreply.github.com>
2020-03-28 18:35:54 +0100

    Deleted duplicates

--- tankmensional_data.py
+++ tankmensional_data.py
@@ -8,6 +8,7 @@ Created on Sat Mar 28 16:46:09 2020
 
 import pandas as pd
 import seaborn as sns
+import sklearn
 
 #Constants
 
@@ -15,7 +16,7 @@ ro=1000
 densitat=ro
 viscositat=0.00102
 
-df=pd.read_csv("rowdy.csv", sep=";")
+df=pd.read_csv("db_rowdy.csv", sep=";")
 
 df.rename(columns={'V (rpm)': 'velocitat',"Power (W)":"power"},inplace=True)
 
@@ -26,6 +27,10 @@ sns.scatterplot(x=df["velocitat"], y=df["power"])
 df["Re"]=ro*df["velocitat"]*df["Rodet Diameter"]/viscositat
 
 df["Np"]=(df["power"]/(df["Rodet Diameter"]**5*df["velocitat"]**3*ro))
+
+
+
+
 
 
 
