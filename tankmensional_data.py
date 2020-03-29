12aabc68e1e234e4f009149850207d27bdbaf206
a96lex <62766970+a96lex@users.noreply.github.com>
2020-03-29 04:07:01 +0200

    Update tankmensional_data.py

--- tankmensional_data.py
+++ tankmensional_data.py
@@ -5,7 +5,6 @@ Created on Sat Mar 28 16:46:09 2020
 @author: crull
 """
 
-
 import pandas as pd
 import seaborn as sns
 import sklearn
@@ -13,32 +12,65 @@ import numpy
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+import pymongo
+from pymongo import MongoClient
 
 #Constants
 
 ro=1000
 densitat=ro
 viscositat=0.00102
+mu=viscositat
 
-df=pd.read_csv("db_rowdy.csv", sep=";")
+#connectar a mongodb
+
+client=MongoClient('mongodb+srv://Alex:Alex@tankmensional-v6eso.mongodb.net/test?retryWrites=true&w=majority')
+db = client['tankmension']
+collection = db['dades']
+
+
+#crear la base de dades de nou
+
+pd.options.display.max_columns = None
+
+df = pd.DataFrame(list(collection.find({})))
 
 
 df.rename(columns={'V (rpm)': 'velocitat',"Power (W)":"power"},inplace=True)
 
 ##df=df[df["Rodet"]=="Helix"]
 
-df["velocitat"]=df["velocitat"]/60
+a=[]
+b=[]
+c=[]
+d=[]
+
+for i in range(0,322):
+    a.append('')
+    b.append('')
+    c.append('')
+    d.append('')
+
+df['Re']=a
+df['Np']=b
+df
+df['logRe']=c
+df['logNp']=d
+
+for i in range(0,322):
+    df["velocitat"][i]=float(df["velocitat"][i])
+    df["Rodet Diameter"][i]=float(df["Rodet Diameter"][i])
+    df["velocitat"][i]=float(df["velocitat"][i])
+    df["power"][i]=float(df["power"][i])
+    df["velocitat"][i]=df["velocitat"][i]/60
+
+for i in range(0,322):    
+    df["Re"][i]=ro*df["velocitat"][i]*df["Rodet Diameter"][i]/viscositat
+    df["Np"][i]=(df["power"][i])/(df["Rodet Diameter"][i]**5*df["velocitat"][i]**3*ro)
+    df["logRe"][i]=numpy.log(int(df["Re"][i]))
+    df["logNp"][i]=numpy.log(int(df["Np"][i]))
 
 sns.scatterplot(x=df["velocitat"], y=df["power"])
-
-df["Re"]=ro*df["velocitat"]*df["Rodet Diameter"]/viscositat
-
-df["Np"]=(df["power"]/(df["Rodet Diameter"]**5*df["velocitat"]**3*ro))
-
-
-
-df["logRe"]=numpy.log(df["Re"])
-df["logNp"]=numpy.log(df["Np"])
 
 
 for rodet in df["Rodet"].unique():
@@ -75,9 +107,6 @@ p=df["power"]
 phat=Nphat*df["Rodet Diameter"]**5*df["velocitat"]**3*ro
 
 df[["Circular", 'Disc', 'Helix', "Turbina"]] = pd.get_dummies(df["Rodet"])
-
-
-
 
 
 
